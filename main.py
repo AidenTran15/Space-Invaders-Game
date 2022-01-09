9cd4552caf177c48191ddfac03378700130b6acc
Aiden Tran <aidenkiettran@gmail.com>
2022-01-09 17:54:06 +1100

    added ship key move function

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ def main():
     level = 1
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+    player_vel = 5
     ship = Ship(300, 650)
 
     clock = pygame.time.Clock()
@@ -69,5 +70,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]: # left
+            ship.x -= player_vel
+        if keys[pygame.K_d]: # right
+            ship.x += player_vel
+        if keys[pygame.K_w]: # up
+            ship.y -= player_vel
+        if keys[pygame.K_s]: # down
+            ship.y += player_vel
 
 main()
